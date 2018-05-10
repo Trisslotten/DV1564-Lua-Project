0fd5e3ac33c59c345fb487b8e7656e1117f10b77
Trisslotten <tristan23@live.se>
2018-05-10 18:31:56 +0200

    added fps cam and testing mesh from vertices

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,6 +12,9 @@
 #include "lua.hpp"
 #include <irrlicht.h>
 
+#include <vector>
+#include <numeric>
+
 void ConsoleThread(lua_State* L)
 {
 	char command[1000];
@@ -24,14 +27,52 @@ void ConsoleThread(lua_State* L)
 	}
 }
 
+
+
+irr::scene::SMesh* createMesh(std::vector<irr::core::vector3df> vertices)
+{ 
+
+	irr::scene::SMesh* mesh = new irr::scene::SMesh();
+	irr::scene::SMeshBuffer* buffer = new irr::scene::SMeshBuffer();
+
+	int size = vertices.size() - vertices.size() % 3;
+
+	buffer->Vertices.set_used(size);
+	for (int i = 0; i < size; i++)
+	{
+		auto& vert = buffer->Vertices[i];
+		vert.Pos.set(vertices[i]);
+		//vert.Normal.set(0, 1, 0);
+		//vert.Color.set(255, 255, 0, 0);
+		vert.TCoords.set(vertices[i].X, vertices[i].Y);
+	}
+
+	buffer->Indices.set_used(size);
+	for (int i = 0; i < size; i++)
+	{
+		buffer->Indices[i] = i;
+	}
+
+	mesh->addMeshBuffer(buffer);
+	mesh->setDirty();
+	mesh->recalculateBoundingBox();
+	return mesh;
+}
+
+
+
+
+
+
 int main()
 {
+
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 
 	std::thread conThread(ConsoleThread, L);
 
-	irr::IrrlichtDevice* device = irr::createDevice(irr::video::EDT_SOFTWARE, irr::core::dimension2d<irr::u32>(640, 480), 16, false, false, true, 0);
+	irr::IrrlichtDevice* device = irr::createDevice(irr::video::EDT_SOFTWARE, irr::core::dimension2d<irr::u32>(1280, 720), 16, false, false, true, 0);
 	if (!device)
 		return 1;
 
@@ -42,14 +83,54 @@ int main()
 
 	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!", irr::core::rect<irr::s32>(10, 10, 260, 22), true);
 
+	irr::video::SMaterial bogdan;
+
+	std::vector<irr::core::vector3df> vertices;
+	for (int i = 0; i < 3 * 1; i++)
+	{
+		float x = 2.f*float(rand()) / RAND_MAX;
+		float y = 2.f*float(rand()) / RAND_MAX;
+		float z = 2.f*float(rand()) / RAND_MAX;
+		vertices.emplace_back(x,y,z);
+	}
+
+	auto meshNode = smgr->addMeshSceneNode(createMesh(vertices));
+
+	meshNode->setMaterialFlag(irr::video::EMF_BACK_FACE_CULLING, false);
+	meshNode->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+	meshNode->setMaterialTexture(0, driver->getTexture("../Meshes/sydney.bmp"));
+
+
+	irr::SKeyMap keys[4];
+	keys[0].Action = irr::EKA_MOVE_FORWARD;
+	keys[0].KeyCode = irr::KEY_KEY_W;
+
+	keys[1].Action = irr::EKA_MOVE_BACKWARD;
+	keys[1].KeyCode = irr::KEY_KEY_S;
+
+	keys[2].Action = irr::EKA_STRAFE_LEFT;
+	keys[2].KeyCode = irr::KEY_KEY_A;
+
+	keys[3].Action = irr::EKA_STRAFE_RIGHT;
+	keys[3].KeyCode = irr::KEY_KEY_D;
+
+	auto cam = smgr->addCameraSceneNodeFPS(0, 100.f, 0.02f, -1, keys, 4);
+	
+
 	while (device->run())
 	{
+		bool windowActive = device->isWindowActive();
+		cam->setInputReceiverEnabled(windowActive);
+		device->getCursorControl()->setVisible(!windowActive);
+
+
 		driver->beginScene(true, true, irr::video::SColor(255, 90, 101, 140));
 
 		smgr->drawAll();
 		guienv->drawAll();
 
 		driver->endScene();
+
 	}
 
 	device->drop();

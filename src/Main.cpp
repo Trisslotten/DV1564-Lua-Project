e6e8b410aab5fddb4bb929ba55b8a4f94010c28a
Trisslotten <tristan23@live.se>
2018-05-14 23:41:03 +0200

    added addBox and getNodes for lua and texture coords for meshes

--- src/Main.cpp
+++ src/Main.cpp
@@ -17,6 +17,7 @@
 #include <optional>
 #include <string>
 #include <mutex>
+#include <cassert>
 
 
 void ConsoleThread(lua_State* L)
@@ -40,7 +41,7 @@ int generateGUID()
 
 
 
-irr::scene::SMesh* createMesh(std::vector<irr::core::vector3df> vertices)
+irr::scene::SMesh* createMesh(std::vector<irr::core::vector3df> vertices, std::vector<irr::core::vector2df> texCoords)
 { 
 
 	irr::scene::SMesh* mesh = new irr::scene::SMesh();
@@ -48,14 +49,46 @@ irr::scene::SMesh* createMesh(std::vector<irr::core::vector3df> vertices)
 
 	int size = vertices.size() - vertices.size() % 3;
 
+	assert(vertices.size() == texCoords.size() || texCoords.empty());
+
 	buffer->Vertices.set_used(size);
 	for (int i = 0; i < size; i++)
 	{
 		auto& vert = buffer->Vertices[i];
 		vert.Pos.set(vertices[i]);
 		//vert.Normal.set(0, 1, 0);
-		//vert.Color.set(255, 255, 0, 0);
-		vert.TCoords.set(vertices[i].X, vertices[i].Y);
+		vert.Color.set(255, 255, 255, 255);
+		if (!texCoords.empty())
+		{
+			vert.TCoords.set(texCoords[i]);
+		}
+		else
+		{
+			switch (i % 3)
+			{
+			case 0:
+				vert.TCoords.set(0, 0);
+				break;
+
+			case 1:
+				{
+					auto first = buffer->Vertices[i - 1].Pos;
+					auto diff = first - vert.Pos;
+					float length = diff.getLength();
+					vert.TCoords.set(length, 0);
+					break;
+				}
+			case 2:
+				{
+					auto first = buffer->Vertices[i - 2].Pos;
+					auto diff = first - vert.Pos;
+					float length = diff.getLength();
+					vert.TCoords.set(0, length);
+					break;
+				}
+			}
+		}
+
 	}
 
 	buffer->Indices.set_used(size);
@@ -72,7 +105,7 @@ irr::scene::SMesh* createMesh(std::vector<irr::core::vector3df> vertices)
 
 
 
-void addMesh(irr::IrrlichtDevice* device, std::vector<irr::core::vector3df> vertices, std::optional<std::string> name = std::nullopt)
+void addMesh(irr::IrrlichtDevice* device, std::vector<irr::core::vector3df> vertices, std::vector<irr::core::vector2df> texCoords, std::optional<std::string> name = std::nullopt)
 {
 	int id = generateGUID();
 	if (!name)
@@ -84,12 +117,12 @@ void addMesh(irr::IrrlichtDevice* device, std::vector<irr::core::vector3df> vert
 	irr::video::IVideoDriver* driver = device->getVideoDriver();
 	irr::scene::ISceneManager* smgr = device->getSceneManager();
 
-	auto meshNode = smgr->addMeshSceneNode(createMesh(vertices), 0, id);
+	auto meshNode = smgr->addMeshSceneNode(createMesh(vertices, texCoords), 0, id);
 	meshNode->setName(name.value().c_str());
 
 	meshNode->setMaterialFlag(irr::video::EMF_BACK_FACE_CULLING, false);
 	meshNode->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-	meshNode->setMaterialTexture(0, driver->getTexture("../Meshes/sydney.bmp"));
+	//meshNode->setMaterialTexture(0, driver->getTexture("../Meshes/sydney.bmp"));
 }
 
 
@@ -135,7 +168,20 @@ void addBox(irr::IrrlichtDevice* device, irr::core::vector3df pos, float size, s
 	{
 		vertices[i] = size * vertices[i] / 2 + pos;
 	}
-	addMesh(device, vertices, name);
+
+	std::vector<irr::core::vector2df> texCoords;
+	texCoords.resize(6 * 6);
+	for (int i = 0; i < 6*6; i += 6)
+	{
+		texCoords[i].set(0, 0);
+		texCoords[i+1].set(1, 1);
+		texCoords[i+2].set(0, 1);
+		texCoords[i + 3].set(0, 0);
+		texCoords[i + 4].set(1, 0);
+		texCoords[i + 5].set(1, 1);
+	}
+
+	addMesh(device, vertices, texCoords, name);
 }
 
 
@@ -143,6 +189,7 @@ struct NodeInfo
 {
 	int id;
 	std::string name;
+	std::string type;
 };
 
 std::vector<NodeInfo> getNodes(irr::scene::ISceneManager* smgr)
@@ -179,13 +226,10 @@ void snapshot(const std::string filename)
 static int lc_snapshot(lua_State* L)
 {
 	if (lua_gettop(L) != 1)
-	{
 		return luaL_error(L, "expected single argument");
-	}
+
 	if (!lua_isstring(L, -1))
-	{
 		return luaL_argerror(L, 1, "argument not string");
-	}
 
 	auto path = lua_tostring(L, -1);
 	std::cout << path << "\n";
@@ -196,71 +240,64 @@ static int lc_snapshot(lua_State* L)
 }
 
 
+void setVec(irr::core::vector3df& vec, int i, float value)
+{
+	switch (i)
+	{
+	case 0:
+		vec.X = value;
+		break;
+	case 1:
+		vec.Y = value;
+		break;
+	case 2:
+		vec.Z = value;
+		break;
+	}
+}
+
 static int lc_camera(lua_State* L)
 {
 	if (lua_gettop(L) != 2)
-	{
 		return luaL_error(L, "Error: expected two argument");
-	}
-	for (int i = 0; i < 2; i++)
-	{
-		if (!lua_istable(L, i + 1))
-		{
-			return luaL_argerror(L, i + 1, "argument not table");
-		}
-	}
 
-	lua_len(L, -1);
-	int size = lua_tonumber(L, -1);
+	for (int i = 0; i < 2; i++)
+		if (!lua_istable(L, i + 1))
+			return luaL_argerror(L, i + 1, "argument not table");
+
+
+	lua_len(L, 1);
+	int tableSize = lua_tonumber(L, -1);
 	lua_pop(L, 1);
-	if (size != 3)
-	{
-		return luaL_error(L, "Error: expected 3d coordinate for target");
-	}
-	lua_len(L, -2);
-	size = lua_tonumber(L, -1);
+	if (tableSize != 3)
+		return luaL_argerror(L, 1, "eye not 3d coordinate");
+
+
+	lua_len(L, 2);
+	tableSize = lua_tonumber(L, -1);
 	lua_pop(L, 1);
-	if (size != 3)
-	{
-		return luaL_error(L, "Error: expected 3d coordinate for eye");
-	}
+	if (tableSize != 3)
+		return luaL_argerror(L, 2, "target not 3d coordinate");
+
 
 	irr::core::vector3df eye;
 	irr::core::vector3df target;
 
 	
-	auto setVec = [](irr::core::vector3df& vec, int i, float value)
-	{
-		switch (i)
-		{
-		case 0:
-			vec.X = value;
-			break;
-		case 1:
-			vec.Y = value;
-			break;
-		case 2:
-			vec.Z = value;
-			break;
-		}
-	}; 
-
 	for (int i = 0; i < 3; i++)
 	{
 		lua_rawgeti(L, -1, i + 1);
 		if (!lua_isnumber(L, -1))
-		{
-			return luaL_error(L, "Error: target coordinate not a number");
-		}
+			return luaL_argerror(L, 2, "target contains non number");
+
 		setVec(target, i, lua_tonumber(L, -1));
 		lua_pop(L, 1);
 
 
 		lua_rawgeti(L, -2, i + 1);
 		if (!lua_isnumber(L, -1))
-		{
-			return luaL_error(L, "Error: eye coordinate not a number");
-		}
+			return luaL_argerror(L, 1, "eye contains non number");
+
 		setVec(eye, i, lua_tonumber(L, -1));
 		lua_pop(L, 1);
 	}
@@ -275,6 +312,93 @@ static int lc_camera(lua_State* L)
 	return 0;
 }
 
+
+static int lc_addBox(lua_State* L)
+{
+	int numArgs = lua_gettop(L);
+	if (numArgs < 2 || numArgs > 3)
+		return luaL_error(L, "Error: expected two or three argument");
+
+	if (!lua_istable(L, 1))
+		return luaL_argerror(L, 1, "argument not table");
+
+	lua_len(L, 1);
+	int tableSize = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+	if (tableSize != 3)
+		return luaL_argerror(L, 1, "position not 3d coordinate");
+
+	if (!lua_isnumber(L, 2))
+		return luaL_argerror(L, 2, "argument not number");
+
+	float boxSize = lua_tonumber(L, 2);
+	irr::core::vector3df pos;
+
+	for (int i = 0; i < 3; i++)
+	{
+		lua_rawgeti(L, 1, i + 1);
+		if (!lua_isnumber(L, -1))
+			return luaL_argerror(L, 1, "position contains non number");
+
+		setVec(pos, i, lua_tonumber(L, -1));
+		lua_pop(L, 1);
+	}
+
+	irr::IrrlichtDevice* device = static_cast<irr::IrrlichtDevice*>(
+		lua_touserdata(L, lua_upvalueindex(1))
+	);
+
+	if (numArgs == 3)
+	{
+		if(!lua_isstring(L, 3))
+			return luaL_argerror(L, 3, "argument not string");
+
+		std::string name = lua_tostring(L, 3);
+		addBox(device, pos, boxSize, name);
+	}
+	else
+	{
+		addBox(device, pos, boxSize);
+	}
+
+	return 0;
+}
+
+static int lc_getNodes(lua_State* L)
+{
+	// remove arguments from stack if any
+	lua_pop(L, lua_gettop(L));
+
+	auto smgr = static_cast<irr::scene::ISceneManager*>(
+		lua_touserdata(L, lua_upvalueindex(1))
+	);
+
+	auto nodes = getNodes(smgr);
+
+	// nodes table
+	lua_newtable(L);
+
+	for (int i = 0; i < nodes.size(); i++)
+	{
+		lua_newtable(L);
+		lua_pushstring(L, "id");
+		lua_pushnumber(L, nodes[i].id);
+		lua_settable(L, 2);
+
+		lua_pushstring(L, "name");
+		lua_pushstring(L, nodes[i].name.c_str());
+		lua_settable(L, 2);
+
+		lua_pushstring(L, "type");
+		lua_pushstring(L, nodes[i].type.c_str());
+		lua_settable(L, 2);
+
+		// add created table to the nodes table
+		lua_rawseti(L, 1, i + 1);
+	}
+
+	return 1;
+}
 
 int main()
 {
@@ -296,22 +420,6 @@ int main()
 	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!", irr::core::rect<irr::s32>(10, 10, 260, 22), true);
 
 
-	for (int i = 0; i < 5; i++)
-	{
-		std::vector<irr::core::vector3df> vertices;
-		for (int j = 0; j < 3 * 1; j++)
-		{
-			float x = 2.f*float(rand()) / RAND_MAX + i*2;
-			float y = 2.f*float(rand()) / RAND_MAX;
-			float z = 2.f*float(rand()) / RAND_MAX;
-			vertices.emplace_back(x,y,z);
-		}
-		addMesh(device, vertices);
-	}
-
-	addBox(device, irr::core::vector3df(1,9,3), 3.f);
-
-
 	irr::SKeyMap keys[4];
 	keys[0].Action = irr::EKA_MOVE_FORWARD;
 	keys[0].KeyCode = irr::KEY_KEY_W;
@@ -328,20 +436,21 @@ int main()
 	auto cam = smgr->addCameraSceneNodeFPS(0, 100.f, 0.02f, generateGUID(), keys, 4);
 	cam->setName("camera");
 
-	auto nodes = getNodes(smgr);
-	for (auto info : nodes)
-	{
-		std::cout << "Name: " << info.name << ", ID: " << info.id << "\n";
-	}
-	
 
 	lua_pushcclosure(L, lc_snapshot, 0);
 	lua_setglobal(L, "snapshot"); 
-	
 
+	lua_pushlightuserdata(L, device);
+	lua_pushcclosure(L, lc_addBox, 1);
+	lua_setglobal(L, "addBox");
+	
 	lua_pushlightuserdata(L, cam);
 	lua_pushcclosure(L, lc_camera, 1);
 	lua_setglobal(L, "camera");
+
+	lua_pushlightuserdata(L, smgr);
+	lua_pushcclosure(L, lc_getNodes, 1);
+	lua_setglobal(L, "getNodes");
 
 
 	while (device->run())
